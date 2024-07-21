27ae6f318b340aa86e255d8a49a8616376a6405d
DhirajGadekar <dhirajgadekar629@gmail.com>
2024-07-21 21:26:35 +0530

    episode 1

---
+++ episode1/app.js
@@ -0,0 +1,6 @@
+var heading = React.createElement("h1", { id : "heading" }, "Hello World");
+console.log(heading);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+root.render(heading);
